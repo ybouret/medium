4ce6ebefda656683cbfc798c2e6db3d238cca71f
ybouret <yann.bouret@gmail.com>
2017-12-20 09:46:47 +0100

    simplified code, have to keep serial for I/O

--- medium.cpp
+++ medium.cpp
@@ -1,23 +1,16 @@
 #include "medium.h"
+#if defined(MEDIUM_TEST)
+#include <cstring>
+#include <iostream>
+#include <math.h>
+#endif
 
 Medium::Medium() : _input(NULL),
                    _inputLength(0),
                    _inputCompleted(false),
-                   _words(),
-                   outputQ(),
-                   outputP(),
-                   nodes(NULL)
+                   _words()
 {
     _input = (char *)malloc(maxInputMemory);
-    const unsigned long nodesMemory = maxOutputLength * (unsigned long)sizeof(CharNode);
-    nodes = (CharNode *)malloc(nodesMemory);
-    memset(nodes, 0, nodesMemory);
-
-    for (unsigned i = 0; i < maxOutputLength; ++i)
-    {
-        CharNode *node = &nodes[i];
-        outputP.store(node);
-    }
     resetInput();
 }
 
@@ -34,6 +27,7 @@ void Medium::resetInput()
     memset(_words, 0, sizeof(_words));
 }
 
+#if !defined(MEDIUM_TEST)
 static bool isEOL(const char C)
 {
     return (C == '\n' || C == '\r');
@@ -64,9 +58,10 @@ void Medium::serialEventCallback()
         }
     }
 }
+#endif
 
 static const char __blanks[] = " \t";
-unsigned Medium::findInputWords(const char *sep)
+unsigned Medium::splitInputWords(const char *sep)
 {
     // initialize loop
     unsigned count = 0;
@@ -91,50 +86,9 @@ unsigned Medium::findInputWords(const char *sep)
     return count;
 }
 
-const char *Medium::getInputWord(const unsigned i) const
+const char *Medium::operator[](const unsigned i) const
 {
     return _words[i % maxInputWords];
-}
-
-void Medium::print(const char *fmt, ...)
-{
-    char buff[maxOutputLength];
-    va_list args;
-    va_start(args, fmt);
-    vsnprintf(buff, sizeof(buff) - 1, fmt, args);
-    va_end(args);
-    const unsigned len = strlen(buff);
-
-    // make some room
-    while (outputP.size < len)
-    {
-        if (Serial.availableForWrite())
-        {
-            CharNode *node = outputQ.pop_back();
-            Serial.write(&(node->data), 1);
-            outputP.store(node);
-        }
-    }
-
-    // push data
-    for (unsigned i = 0; i < len; ++i)
-    {
-        CharNode *node = outputP.query();
-        node->data = buff[i];
-        outputQ.push_front(node);
-    }
-}
-
-void Medium::loopCallback()
-{
-    while (outputQ.size > 0)
-    {
-        if (!Serial.availableForWrite())
-            break;
-        CharNode *node = outputQ.pop_back();
-        Serial.write( &(node->data), 1);
-        outputP.store(node);
-    }
 }
 
 float Medium::Triangle(float t, const float T)
--- medium.h
+++ medium.h
@@ -1,7 +1,14 @@
 //! \file
 #ifndef MEDIUM_INCLUDED
 #define MEDIUM_INCLUDED 1
+
+#if !defined(MEDIUM_TEST)
 #include <Arduino.h>
+#define MEDIUM_ASSERT(EXPR)
+#else
+#include <cassert>
+#define MEDIUM_ASSERT(EXPR) assert(EXPR)
+#endif
 
 // timing macros
 //-----------------------------------------------------------------------------
@@ -15,208 +22,210 @@
 
 class Medium
 {
-public:
-  //! generic doubly linked list
-  template <typename NODE>
-  class List
-  {
   public:
-    NODE *head;
-    NODE *tail;
-    unsigned long size;
+#define MEDIUM_CHECK_NODE(node)        \
+    MEDIUM_ASSERT(node != NULL);       \
+    MEDIUM_ASSERT(NULL == node->next); \
+    MEDIUM_ASSERT(NULL == node->prev)
 
-    inline explicit List() throw() : head(NULL), tail(NULL), size(0)
+    //! generic doubly linked list
+    template <typename NODE>
+    class List
     {
-    }
+      public:
+        NODE *head;
+        NODE *tail;
+        unsigned long size;
 
-    inline virtual ~List() throw()
+        inline explicit List() throw() : head(NULL), tail(NULL), size(0)
+        {
+        }
+
+        inline virtual ~List() throw()
+        {
+        }
+
+        inline void push_back(NODE *node) throw()
+        {
+            MEDIUM_CHECK_NODE(node);
+            if (size <= 0)
+            {
+                head = tail = node;
+            }
+            else
+            {
+                tail->next = node;
+                node->prev = tail;
+                tail = node;
+            }
+            ++size;
+        }
+
+        inline void push_front(NODE *node) throw()
+        {
+            MEDIUM_CHECK_NODE(node);
+            if (size <= 0)
+            {
+                head = tail = node;
+            }
+            else
+            {
+                head->prev = node;
+                node->next = head;
+                head = node;
+            }
+            ++size;
+        }
+
+        inline NODE *pop_back() throw()
+        {
+            NODE *node = tail;
+            switch (size)
+            {
+            case 0:
+                MEDIUM_ASSERT(NULL == node);
+                return node;
+            case 1:
+            {
+                head = tail = NULL;
+                size = 0;
+                MEDIUM_CHECK_NODE(node);
+                return node;
+            }
+            default:
+                break;
+            }
+            tail = node->prev;
+            tail->next = NULL;
+            node->prev = NULL;
+            --size;
+            MEDIUM_CHECK_NODE(node);
+            return node;
+        }
+
+        inline NODE *pop_front() throw()
+        {
+            NODE *node = head;
+            switch (size)
+            {
+            case 0:
+                MEDIUM_ASSERT(NULL == node);
+                return node;
+
+            case 1:
+            {
+                head = tail = NULL;
+                size = 0;
+                MEDIUM_CHECK_NODE(node);
+                return node;
+            }
+            default:
+                break;
+            }
+            head = node->next;
+            head->prev = NULL;
+            node->next = NULL;
+            --size;
+            MEDIUM_CHECK_NODE(node);
+            return node;
+        }
+
+      private:
+        List(const List &);
+        List &operator=(const List &);
+    };
+
+    //! generic pool
+    template <typename NODE>
+    class Pool
     {
-    }
+      public:
+        NODE *top;
+        unsigned long size;
 
-    inline void push_back(NODE *node) throw()
+        inline explicit Pool() throw() : top(NULL), size(0) {}
+        inline virtual ~Pool() throw() {}
+
+        inline void store(NODE *node) throw()
+        {
+            MEDIUM_CHECK_NODE(node);
+            node->next = top;
+            top = node;
+            ++size;
+        }
+
+        inline NODE *query() throw()
+        {
+            MEDIUM_ASSERT(size > 0);
+            NODE *node = top;
+            top = top->next;
+            node->next = NULL;
+            --size;
+            MEDIUM_CHECK_NODE(node);
+            return node;
+        }
+
+      private:
+        Pool(const Pool &);
+        Pool &operator=(const Pool &);
+    };
+
+    //! generic node
+    template <typename T>
+    class NodeOf
     {
-      if (size <= 0)
-      {
-        head = tail = node;
-      }
-      else
-      {
-        tail->next = node;
-        node->prev = tail;
-        tail = node;
-      }
-      ++size;
-    }
+      public:
+        NodeOf *next;
+        NodeOf *prev;
+        T data;
 
-    inline void push_front(NODE *node) throw()
-    {
-      if (size <= 0)
-      {
-        head = tail = node;
-      }
-      else
-      {
-        head->prev = node;
-        node->next = head;
-        head = node;
-      }
-      ++size;
-    }
+        inline NodeOf() throw() : next(NULL), prev(NULL), data() {}
+        inline NodeOf(const T &args) throw() : next(NULL), prev(NULL), data(args) {}
+        inline ~NodeOf() throw() {}
 
-    inline NODE *pop_back() throw()
-    {
-      NODE *node = tail;
-      switch (size)
-      {
-      case 0:
-        return node;
-      case 1:
-      {
-        head = tail = NULL;
-        size = 0;
-        return node;
-      }
-      default:
-        break;
-      }
-      tail = node->prev;
-      tail->next = NULL;
-      node->prev = NULL;
-      --size;
-      return node;
-    }
+      private:
+        NodeOf(const NodeOf &);
+        NodeOf &operator=(const NodeOf &);
+    };
 
-    inline NODE *pop_front() throw()
-    {
-      NODE *node = head;
-      switch (size)
-      {
-      case 0:
-        return node;
+    static const unsigned maxInputLength = 255;
+    static const unsigned maxInputMemory = maxInputLength + 1;
+    static const unsigned maxInputWords = 4;
 
-      case 1:
-      {
-        head = tail = NULL;
-        size = 0;
-        return node;
-      }
-      default:
-        break;
-      }
-      head = node->next;
-      head->prev = NULL;
-      node->next = NULL;
-      --size;
-      return node;
-    }
+    //! check an input is ready
+    inline bool inputCompleted() const { return _inputCompleted; }
+
+    //! get read only current input
+    inline const char *input() const { return _input; }
+
+    //! get read only current input length
+    inline unsigned inputLength() const { return _inputLength; }
+
+    //! to be called within serialEvent() function
+    void serialEventCallback();
+    void resetInput(); //!< hard reset input status
+    //! tokenize input into 0..maxInputArgs
+    /**
+     * \param sep if NULL, blanks are used
+     */
+    unsigned splitInputWords(const char *sep = NULL);
+    const char * operator[](const unsigned i) const;
+
+    //! prepare an object for I/O
+    Medium();
+    ~Medium() throw();
+
+    static float Triangle(float t, const float T);
+    static float SineWave(float t, const float T);
 
   private:
-    List(const List &);
-    List &operator=(const List &);
-  };
+    char *_input;                //!< allocated input buffer
+    unsigned _inputLength;       //!< current length
+    bool _inputCompleted;        //!< end of line, or max size reached
+    char *_words[maxInputWords]; //!< split words from input
 
-  //! generic pool
-  template <typename NODE>
-  class Pool
-  {
-  public:
-    NODE *top;
-    unsigned long size;
-
-    inline explicit Pool() throw() : top(NULL), size(0) {}
-    inline virtual ~Pool() throw() {}
-
-    inline void store(NODE *node) throw()
-    {
-      node->next = top;
-      top = node;
-      ++size;
-    }
-
-    inline NODE *query() throw()
-    {
-      //assume size>0
-      NODE *node = top;
-      top = top->next;
-      node->next = NULL;
-      --size;
-      return node;
-    }
-
-  private:
-    Pool(const Pool &);
-    Pool &operator=(const Pool &);
-  };
-
-  //! generic node
-  template <typename T>
-  class NodeOf
-  {
-  public:
-    NodeOf *next;
-    NodeOf *prev;
-    T data;
-
-    inline NodeOf() throw() : next(NULL), prev(NULL), data() {}
-    inline NodeOf(const T &args) throw() : next(NULL), prev(NULL), data(args) {}
-    inline ~NodeOf() throw() {}
-
-  private:
-    NodeOf(const NodeOf &);
-    NodeOf &operator=(const NodeOf &);
-  };
-
-  static const unsigned maxInputLength = 255;
-  static const unsigned maxInputMemory = maxInputLength + 1;
-  static const unsigned maxInputWords = 4;
-  static const unsigned maxOutputLength = 128;
-
-  //! check an input is ready
-  inline bool inputCompleted() const { return _inputCompleted; }
-
-  //! get read only current input
-  inline const char *input() const { return _input; }
-
-  //! get read only current input length
-  inline unsigned inputLength() const { return _inputLength; }
-
-  //! to be called within serialEvent() function
-  void serialEventCallback();
-  void resetInput(); //!< hard reset input status
-  //! tokenize input into 0..maxInputArgs
-  /**
-   * \param sep if NULL, blanks are used 
-   */
-  unsigned findInputWords(const char *sep = NULL);
-  const char *getInputWord(const unsigned i) const;
-
-  void loopCallback();
-
-  //! print a formatted message, must be outputReady
-  void print(const char *fmt, ...);
-
-  //! prepare an object for I/O
-  Medium();
-  ~Medium() throw();
-
-  static float Triangle(float t, const float T);
-  static float SineWave(float t, const float T);
-
-private:
-  char *_input;                //!< allocated input buffer
-  unsigned _inputLength;       //!< current length
-  bool _inputCompleted;        //!< end of line, or max size reached
-  char *_words[maxInputWords]; //!< split words from input
-
-  typedef NodeOf<char> CharNode;
-  typedef List<CharNode> CharList;
-  typedef Pool<CharNode> CharPool;
-  CharList outputQ;
-  CharPool outputP;
-  CharNode *nodes;
-
-  Medium(const Medium &);
-  Medium &operator=(const Medium &);
+    Medium(const Medium &);
+    Medium &operator=(const Medium &);
 };
 
 #endif
